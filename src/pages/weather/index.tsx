99451a2d2539631b27f4920569c568859e69bbcd
dasom <ds92ko@gmail.com>
2026-02-05 05:59:38 +0900

    feat: 날씨 상세 페이지 추가

--- src/pages/weather/index.tsx
+++ src/pages/weather/index.tsx
@@ -1,9 +1,44 @@
+import useLocationWeather from '@/features/weather/lib/use-location-weather'
+import NotFound from '@/pages/not-found'
+import DISTRICTS from '@/shared/data/korea_districts.json' with { type: 'json' }
+import useTitle from '@/shared/lib/use-title'
+import FavoriteToolbar from '@/widgets/favorite/ui/favorite-toolbar'
+import HourlyOverview from '@/widgets/weather/ui/hourly-overview'
+import LocationWeather from '@/widgets/weather/ui/location-weather'
+import TempDistribution from '@/widgets/weather/ui/temp-distribution-section'
 import { useParams } from 'react-router-dom'
 
 const Weather = () => {
   const { location } = useParams<{ location: string }>()
+  const { placeName, weather, isLoading, isError } = useLocationWeather(
+    location || ''
+  )
 
-  return <>Weather {location}</>
+  useTitle(placeName)
+
+  if (!location || !DISTRICTS.includes(location)) return <NotFound />
+
+  return (
+    <>
+      <FavoriteToolbar location={location} />
+      <LocationWeather
+        placeName={placeName}
+        weather={weather}
+        isLoading={isLoading}
+        isError={isError}
+      />
+      <HourlyOverview
+        hourly={weather?.hourly}
+        isLoading={isLoading}
+        isError={isError}
+      />
+      <TempDistribution
+        weather={weather}
+        isLoading={isLoading}
+        isError={isError}
+      />
+    </>
+  )
 }
 
 export default Weather
