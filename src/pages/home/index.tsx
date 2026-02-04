90c39a510d8c8f4b91f3fa4c0cbe33bf851a4ab9
dasom <ds92ko@gmail.com>
2026-02-04 20:58:02 +0900

    feat: 홈페이지 컴포넌트 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,21 @@
+import useTitle from '@/shared/lib/use-title'
+import FavoriteList from '@/widgets/favorite/ui/favorite-list'
+import LocationSearch from '@/widgets/location/ui/location-search'
+import CurrentWeather from '@/widgets/weather/ui/current-weather'
+import { useRef } from 'react'
+
 const Home = () => {
-  return <>Home</>
+  useTitle()
+  const searchRef = useRef<HTMLInputElement>(null)
+  const focusSearch = () => searchRef.current?.focus()
+
+  return (
+    <>
+      <LocationSearch ref={searchRef} />
+      <CurrentWeather />
+      <FavoriteList onSearchFocus={focusSearch} />
+    </>
+  )
 }
 
 export default Home
