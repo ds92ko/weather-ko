3385b81d9a30307c7ce3a0ab2a6af6d07520115f
dasom <ds92ko@gmail.com>
2026-02-05 09:06:01 +0900

    perf: 쿼리 캐시 시간 조정

--- src/shared/api/query-client.ts
+++ src/shared/api/query-client.ts
@@ -4,8 +4,8 @@ import { QueryClient } from '@tanstack/react-query'
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5,
-      gcTime: 1000 * 60 * 10,
+      staleTime: 1000 * 60 * 15,
+      gcTime: 1000 * 60 * 30,
       retry: (failureCount, error) => {
         if (
           error instanceof ApiError &&
